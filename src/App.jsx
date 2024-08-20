dd09603120f0636a7bf659eda1a5b0c3fa48296f
ianmark8 <ianmarkmorga07@gmail.com>
2024-08-20 14:45:47 +0800

    Create home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
-import Home from "./components/Home";
+import Home from "./pages/Home";
 
 function App() {
   return (
