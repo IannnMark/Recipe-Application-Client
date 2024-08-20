85fa4b97cc28a8b3096300733da9b7db5c5d178e
ianmark8 <ianmarkmorga07@gmail.com>
2024-08-20 12:54:15 +0800

    create header ui

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,34 @@
-
+import { FaSearch } from "react-icons/fa";
 
 export default function Header() {
   return (
-  <header className=""></header>
-  )
+    <header className="">
+      <div className="flex justify-between items-center max-w-6xl max-auto p-3">
+        <h1 className="font-bold text-lg sm:text-xl flex flex-wrap gap-2">
+          <span className="text-slate-900">Recipe</span>
+          <span className="text-slate-900">Application</span>
+        </h1>
+
+        <form className="">
+          <input
+            type="text"
+            placeholder="Search...."
+            className="bg-transparent focus:outline-none w-24 sm:w-64"
+          />
+          <button>
+            <FaSearch className="text-slate-900" />
+          </button>
+        </form>
+        <ul className="flex gap-3">
+          <li className="hidden sm:inline text-slate-900 hover:underline">
+            Home
+          </li>
+          <li className="hidden sm:inline text-slate-900 hover:underline">
+            About
+          </li>
+          <li className="text-slate-900 hover:underline">Sign in</li>
+        </ul>
+      </div>
+    </header>
+  );
 }
