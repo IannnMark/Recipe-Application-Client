5dbd92c8e3ba6ae2e36102358b2123157aaac09b
ianmark8 <ianmarkmorga07@gmail.com>
2024-09-22 19:30:09 +0800

    fix the typo

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -71,7 +71,7 @@ export default function SignUp() {
         <button
           disabled={loading}
           className="bg-slate-900 text-white p-3
-         rouned-lg uppercase hover:opacity-95 disabled:opacity-80"
+         rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
         >
           {loading ? "Loading..." : "Sign Up"}
         </button>
